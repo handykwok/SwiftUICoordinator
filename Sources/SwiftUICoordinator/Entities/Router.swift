274e0b29c4ee5ecf210c88e7b5ca981bc94fa695
Handy <handy.kwok@gmail.com>
2024-04-27 12:48:44 +0700

    add final at CoordinatorsFactory class and remove ObservableObject at Router

--- Sources/SwiftUICoordinator/Entities/Router.swift
+++ Sources/SwiftUICoordinator/Entities/Router.swift
@@ -10,9 +10,9 @@ import Foundation
 /**
  A class responsible for routing and managing navigable views.
  */
-public final class Router: ObservableObject {
+public final class Router {
     /// The array of navigable views managed by the router.
-    @Published var navigableViews: [NavigableView] = []
+    var navigableViews: [NavigableView] = []
     
     public init() {}
 }
@@ -23,21 +23,21 @@ extension Router: Routable {
      
      - Parameter view: The navigable view to be presented.
      */
-    func present(_ view: NavigableView) {
+    public func present(_ view: NavigableView) {
         navigableViews.append(view)
     }
     
     /**
      Pops to the root view by removing all views from the navigation stack.
      */
-    func popToRoot() {
+    public func popToRoot() {
         navigableViews.removeAll()
     }
     
     /**
      Pops the current view from the navigation stack.
      */
-    func pop() {
+    public func pop() {
         navigableViews.removeLast()
     }
 }
